c752ddde279bae827d8fe2779932b98882b0dba4
Karl Neyvaldt <karl.neyvaldt@gmail.com>
2017-01-23 23:17:41 +0100

    unless check ignores whitespace

--- manifests/init.pp
+++ manifests/init.pp
@@ -90,7 +90,7 @@ define sysctl (
       $qvalue = shellquote("${value}")
       # lint:endignore
       exec { "enforce-sysctl-value-${qtitle}":
-          unless  => "/usr/bin/test \"$(/sbin/sysctl -n ${qtitle} | tr -d '[:space:]')\" = \"echo ${qvalue} | tr -d '[:space:]'\"",
+          unless  => "/usr/bin/test \"$(/sbin/sysctl -n ${qtitle} | tr -d '[:space:]')\" = \"$(echo ${qvalue} | tr -d '[:space:]')\"",
           command => "/sbin/sysctl -w ${qtitle}=${qvalue}",
       }
     }
